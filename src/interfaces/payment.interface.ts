f2444645c4d7cb903d30dfe09c4ec1afa259e1e1
Peter Kang <peter@niesh.co.nz>
2020-11-25 14:45:08 +1300

    define initial version of Payment interface and PaymentStatus enum

--- src/interfaces/payment.interface.ts
+++ src/interfaces/payment.interface.ts
@@ -1,1 +1,11 @@
-export interface Payment {}
+export interface Payment {
+  dueBy: Date;
+  description: string;
+  status: PaymentStatus;
+  amount: number;
+  id: string;
+}
+
+export enum PaymentStatus {
+  Overdue = 1,
+}
